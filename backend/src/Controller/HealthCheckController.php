19e486fe14bf54f0dcbcd56d0e77e55d581cfb9c
Paweł Piórkowski <pawel.piorkowski@mailmetrics.com>
2025-01-02 15:42:36 +0100

    change react to nextjs

---
+++ backend/src/Controller/HealthCheckController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthCheckController
+{
+    #[Route('/healthcheck', name: 'healthcheck')]
+    public function index(): Response
+    {
+        return new Response('OK', Response::HTTP_OK);
+    }
+}
